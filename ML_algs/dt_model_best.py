a75f2277bc207e08adb8e39498f64d072dfedf4c
Victor Vieira <victorbor2001@yahoo.com.br>
2024-12-08 14:49:05 -0300

    model training using whole dataset

--- ML_algs/dt_model_best.py
+++ ML_algs/dt_model_best.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 
 from sklearn.tree import DecisionTreeClassifier, plot_tree
 
@@ -8,22 +9,26 @@ from sklearn.model_selection import train_test_split, LearningCurveDisplay, lear
 from sklearn.metrics import confusion_matrix, accuracy_score, f1_score
 
 
-chunksize = 5e3
+# chunksize = 5e3
 
-benign_fragment_i = 4
+# benign_fragment_i = 4
 
-df_class_0 = pd.read_csv(f'./fragments/benign_fragment_{benign_fragment_i}_filtered.csv', chunksize=chunksize, delimiter=';')
-df_class_1 = pd.read_csv('./fragments/malware_fragment_filtered.csv', chunksize=chunksize, delimiter=';')
+# df_class_0 = pd.read_csv(f'./fragments/benign_fragment_{benign_fragment_i}_filtered.csv', chunksize=chunksize, delimiter=';')
+# df_class_1 = pd.read_csv('./fragments/malware_fragment_filtered.csv', chunksize=chunksize, delimiter=';')
 
-benigno = next(df_class_0)
-maligno = next(df_class_1)
+# benigno = next(df_class_0)
+# maligno = next(df_class_1)
 
-maligno["CLASS"] = 1
-benigno["CLASS"] = 0
+# maligno["CLASS"] = 1
+# benigno["CLASS"] = 0
 
-data = pd.concat([benigno, maligno], join='outer').fillna(0)
+# data = pd.concat([benigno, maligno], join='outer').fillna(0)
+# data = data.drop(columns=['SHA256', 'NOME', 'PACOTE', 'API_MIN', 'API', "vt_detection", "VT_Malware_Deteccao", "AZ_Malware_Deteccao"])
 
-data = data.drop(columns=['SHA256', 'NOME', 'PACOTE', 'API_MIN', 'API', 'vt_detection', "VT_Malware_Deteccao", "AZ_Malware_Deteccao"])
+data = pd.read_hdf('./data.h5')
+data['CLASS'] = np.where(data['vt_detection'] < 4, 0, 1)
+
+data = data.drop(columns=['SHA256', 'NOME', 'PACOTE', 'API_MIN', 'API', "vt_detection", "VT_Malware_Deteccao", "AZ_Malware_Deteccao"])
 
 X = data.drop(columns=['CLASS'])
 y = data['CLASS']
