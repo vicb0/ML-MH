bdc6bc9157482b26d54615fe47a4cd809cb6595a
Leonardo Josephson <leonardojosephson24@gmail.com>
2025-01-28 16:37:03 -0300

    minor change to avoid column names in code

--- feature_selection.py
+++ feature_selection.py
@@ -1,15 +1,14 @@
 import pandas as pd
-import numpy as np
 
 def feature_selection(file_name, variance):
-    df = pd.read_csv(file_name, sep=';') 
+    df = pd.read_csv(file_name, sep=';')
 
-    df["Variance"]=df["Variance"].str.replace(',','.')
-    df["Variance"] = pd.to_numeric(df["Variance"])
+    df.iloc[:, 1] = df.iloc[:, 1].str.replace(',', '.')
+    df.iloc[:, 1] = pd.to_numeric(df.iloc[:, 1])
 
-    df = df.loc[df['Variance'] < variance]
+    df = df.loc[df.iloc[:, 1] < variance]
 
-    return df["Feature"].tolist()
+    return df.iloc[:, 1].tolist()
 
 
 #print(len(feature_selection('variances1.csv', 0.1)))
