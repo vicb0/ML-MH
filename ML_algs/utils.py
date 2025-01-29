39ddf8297bec5138888d45e2fb48496908534609
Victor Vieira <victorbor2001@yahoo.com.br>
2025-01-29 16:55:41 -0300

    fixed drop_low_var

--- ML_algs/utils.py
+++ ML_algs/utils.py
@@ -15,4 +15,4 @@ def save_results(file_name:str, title:str, content:list):
 
 vrs = pd.read_csv("variances.csv", sep=";", decimal=",")
 def drop_low_var(df, var=0.1):
-    return df.drop(vrs.loc[vrs["variancia"] >= var, "column"], axis=1)
+    return df.drop(vrs.loc[vrs["variancia"] <= var, "column"], axis=1)
