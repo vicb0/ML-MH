6074ff666f81ba5ab82afe070c58c87bf061f288
Victor Vieira <victorbor2001@yahoo.com.br>
2025-02-24 15:47:41 -0300

    .

--- ML_algs/utils.py
+++ ML_algs/utils.py
@@ -2,6 +2,10 @@ import os
 import pandas as pd
 
 
+def drop_metadata(df):
+    return df.drop(columns=['SHA256', 'NOME', 'PACOTE', 'API_MIN', 'API', 'vt_detection', "VT_Malware_Deteccao", "AZ_Malware_Deteccao", 'CLASS'])
+
+
 def save_results(file_name:str, title:str, content:list):
     os.makedirs('./results', exist_ok=True)
 
@@ -17,5 +21,6 @@ vrs = pd.read_csv("variances.csv", sep=";", decimal=",")
 def drop_low_var(df, var=0.1):
     return df.drop(vrs.loc[vrs["variancia"] <= var, "column"], axis=1)
 
-def drop_low_var_by_col(df, col=4000):
+
+def drop_low_var_by_col(df, col=4000) -> pd.DataFrame:
     return df.drop(vrs.nsmallest(len(df.columns) - col, 'variancia')["column"], axis=1)
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import numpy
 
+
 # run the following command: `git clone https://github.com/Malware-Hunter/MH-1M`
 # navigate to data/compressed/zip-intents-permissions-opcodes-apicalls
 # run the command `copy /b amex-1M-[intents-permissions-opcodes-apicalls].npz.7z.part* full_archive.7z`
@@ -24,46 +25,93 @@ for c, line in enumerate(lines1):
     else:
         lines1[c] = f"{category}::{column}"
 
-
 lines1.sort()
 with open('./MH-1M/headers.txt', 'w') as f:
     f.write("\n".join(lines1))
 #############################################################################################
-# Convert the headers from MH-100K to a single file, sort and save it
-with open('./headers/apicall.txt', 'r') as f1:
-    with open('./headers/intent.txt', 'r') as f2:
-        with open('./headers/permission.txt', 'r') as f3:
-            with open('./headers/headers.txt', 'w') as f4:
-                lines2 = []
-                lines2.extend(f1.read().lower().split("\n"))
-                lines2.extend(f2.read().lower().split("\n"))
-                lines2.extend(f3.read().lower().split("\n"))
-                lines2.sort()
-                f4.write("\n".join(lines2))
-#############################################################################################
-# Compare the two files
-p1, p2 = 0, 0
-logs = []
-# lines1 = MH-1M
-# lines2 = MH-100K
-while p1 < len(lines1) and p2 < len(lines2):
-    if lines1[p1] > lines2[p2]:
-        logs.append(f"{lines2[p2]} from MH-100K not in MH-1M")
-        p2 += 1
-    elif lines1[p1] < lines2[p2]:
+
+
+def headers_diff():
+    # Convert the headers from MH-100K to a single file, sort and save it
+    with open('./headers/apicall.txt', 'r') as f1:
+        with open('./headers/intent.txt', 'r') as f2:
+            with open('./headers/permission.txt', 'r') as f3:
+                with open('./headers/headers.txt', 'w') as f4:
+                    lines2 = []
+                    lines2.extend(f1.read().lower().split("\n"))
+                    lines2.extend(f2.read().lower().split("\n"))
+                    lines2.extend(f3.read().lower().split("\n"))
+                    lines2.sort()
+                    f4.write("\n".join(lines2))
+    #############################################################################################
+    # Compare the two files
+    p1, p2 = 0, 0
+    logs = []
+    # lines1 = MH-1M
+    # lines2 = MH-100K
+    while p1 < len(lines1) and p2 < len(lines2):
+        if lines1[p1] > lines2[p2]:
+            logs.append(f"{lines2[p2]} from MH-100K not in MH-1M")
+            p2 += 1
+        elif lines1[p1] < lines2[p2]:
+            logs.append(f"{lines1[p1]} from MH-1M not in MH-100K")
+            p1 += 1
+        else:
+            p1 += 1
+            p2 += 1
+
+    while p1 < len(lines1):
         logs.append(f"{lines1[p1]} from MH-1M not in MH-100K")
         p1 += 1
-    else:
+    while p2 < len(lines2):
+        logs.append(f"{lines2[p2]} from MH-100K not in MH-1M")
+        p2 += 1
+    #############################################################################################
+
+    with open('headers_diff.txt', 'w') as f:
+        f.write("\n".join(logs))
+
+
+def headers_diff_highest_variances():
+    import pandas as pd
+    from ML_algs.utils import drop_low_var_by_col
+    from ML_algs.utils import drop_metadata
+
+    df = pd.read_hdf('./dataset.h5')
+    df = drop_low_var_by_col(drop_metadata(df))
+    lines2 = sorted(map(lambda x: x.lower(), df.columns.to_list()))
+
+    # Compare the two files
+    p1, p2 = 0, 0
+    logs = []
+    # lines1 = MH-1M
+    # lines2 = MH-100K (4000 highest variances)
+    while p1 < len(lines1) and p2 < len(lines2):
+        if lines1[p1] > lines2[p2]:
+            logs.append(f"{lines2[p2]} from MH-100K not in MH-1M")
+            p2 += 1
+        elif lines1[p1] < lines2[p2]:
+            logs.append(f"{lines1[p1]} from MH-1M not in MH-100K")
+            p1 += 1
+        else:
+            p1 += 1
+            p2 += 1
+
+    while p1 < len(lines1):
+        logs.append(f"{lines1[p1]} from MH-1M not in MH-100K")
         p1 += 1
+    while p2 < len(lines2):
+        logs.append(f"{lines2[p2]} from MH-100K not in MH-1M")
         p2 += 1
 
-while p1 < len(lines1):
-    logs.append(f"{lines1[p1]} from MH-1M not in MH-100K")
-    p1 += 1
-while p2 < len(lines2):
-    logs.append(f"{lines2[p2]} from MH-100K not in MH-1M")
-    p2 += 1
-#############################################################################################
+    with open('headers_diff_highest_variances.txt', 'w') as f:
+        f.write("\n".join(logs))
 
-with open('headers_diff.txt', 'w') as f:
-    f.write("\n".join(logs))
+
+def main():
+    # headers_diff()
+   headers_diff_highest_variances() 
+
+
+if __name__ == "__main__":
+    main()
