07006f7aa61f3bbd9f81114402a915caaf7fc3bd
Leonardo Josephson <leonardojosephson24@gmail.com>
2025-04-11 16:04:26 -0300

    add confusion matrix plot for xgb model

--- ML_algs/xgb_model.py
+++ ML_algs/xgb_model.py
@@ -2,11 +2,12 @@ import pandas as pd
 import numpy as np
 
 from sklearn.model_selection import train_test_split, StratifiedKFold, GridSearchCV
-from sklearn.metrics import classification_report, accuracy_score
+from sklearn.metrics import classification_report, accuracy_score, ConfusionMatrixDisplay
 
 from utils import drop_low_var_by_col_100k, drop_low_var_100k
 
 import xgboost as xgb
+import matplotlib.pyplot as plt
 
 def GB(data, variance, col):
 
@@ -16,7 +17,6 @@ def GB(data, variance, col):
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.15, random_state=42)
 
     depth={
-
         'base_score':[0.2,0.5,0.7],
         'gamma':[0],
         'booster': ['gbtree'],
@@ -46,13 +46,21 @@ def GB(data, variance, col):
     print(f"Accuracy best model: {accuracy}")
 
     results = pd.DataFrame.from_dict(GB.cv_results_)
+    print(results)
+    print(classification_report(y_test, y_pred))
+    #Save results
     if col:
         results.to_csv(f'xgb_cv_results{col}.csv', sep=';', index=False)  
     else:
-        results.to_csv(f'xgb_cv_results{variance}.csv', sep=';', index=False)  
-    print(results)
-    
-    print(classification_report(y_test, y_pred))
+        results.to_csv(f'xgb_cv_results{variance}.csv', sep=';', index=False)
+
+    plt.rcParams.update({'font.size': 20})
+    ax = plt.subplot()
+    ConfusionMatrixDisplay.from_predictions(y_true=y_test, y_pred=y_pred, ax=ax, cmap="gray", display_labels=['benigno', "maligno"], colorbar=False)
+    ax.set_xlabel("Rótulo previsto", fontsize=18)
+    ax.set_ylabel("Rótulo real", fontsize=18) 
+    ax.plot()
+    plt.show()
 
 
 def main(variance=0.1, col=0):
