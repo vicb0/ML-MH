21ec300ef1fec8ac9d078a87aa069291bdd8c2ef
Leonardo Josephson <leonardojosephson24@gmail.com>
2025-04-13 23:32:20 -0300

    add last rf version

--- ML_algs/rf_model.py
+++ ML_algs/rf_model.py
@@ -1,14 +1,15 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 import pickle
 
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split, StratifiedKFold, GridSearchCV
-from sklearn.metrics import classification_report, accuracy_score
+from sklearn.metrics import classification_report, accuracy_score, precision_score, recall_score, ConfusionMatrixDisplay
 
 from utils import drop_low_var_by_col_100k
 
-def RF(data, variance):
+def RF(data, variance=0.01, col=4000):
 
     X = data.drop(columns=['CLASS'])
     y = data['CLASS']
@@ -18,28 +19,20 @@ def RF(data, variance):
     
     kf = StratifiedKFold(n_splits=4, shuffle=True, random_state=42) # 75% teste e 25% validação
     
-    # params ={
-    #     "n_estimators": [50, 100, 250], #Número de árvores na floresta
-    #     "criterion": ["gini", "entropy", "log_loss"], #Medição da qualidade de uma decisão tomada em um nó
-    #     "max_depth":[10, 20, 50, 100, 150, 200], # altura máxima da árvore
-    #     "min_samples_split":[8, 16, 32, 48], # Número mínimo de registros para dividir um nó em outro
-    #     "min_samples_leaf":[2, 4, 8, 16, 32], # Número mínimo de folhas
-    # }
-
     params ={
-        "n_estimators": [100], #Número de árvores na floresta
-        "criterion": ["log_loss"], #Medição da qualidade de uma decisão tomada em um nó
-        "max_depth":[50], # altura máxima da árvore
-        "min_samples_split":[8], # Número mínimo de registros para dividir um nó em outro
-        "min_samples_leaf":[2], # Número mínimo de folhas
+        "n_estimators": [50, 100, 250], #Número de árvores na floresta
+        "criterion": ["gini", "entropy", "log_loss"], #Medição da qualidade de uma decisão tomada em um nó
+        "max_depth":[10, 20, 50, 100, 150, 200], # altura máxima da árvore
+        "min_samples_split":[8, 16, 32, 48], # Número mínimo de registros para dividir um nó em outro
+        "min_samples_leaf":[2, 4, 8, 16, 32], # Número mínimo de folhas
     }
 
     rf = GridSearchCV(
-        estimator = RandomForestClassifier(),
+        estimator = RandomForestClassifier(random_state=42),
         param_grid = params,
         cv = kf,
         verbose=2,
-        n_jobs=8,
+        n_jobs=4,
     )
 
     rf.fit(X_train, y_train)
@@ -50,30 +43,36 @@ def RF(data, variance):
         pickle.dump(best_model, f)
 
     y_pred = best_model.predict(X_test)
-    accuracy = accuracy_score(y_test, y_pred)
-    print(f"Accuracy best model: {accuracy}") 
+
+    print(f"Accuracy best model: {accuracy_score(y_test, y_pred)}")
+    print(f"Precision best model: {precision_score(y_test, y_pred)}")
+    print(f"Recall best model: {recall_score(y_test, y_pred)}")
 
     results = pd.DataFrame.from_dict(rf.cv_results_)
-    #results.to_csv(f'random_forest_cv_results{variance}.csv', sep=';', index=False)  
+    results.to_csv(f'random_forest_cv_results{col}.csv', sep=';', index=False)  
     print(results)
     print(classification_report(y_test, y_pred))
 
+    plt.rcParams.update({'font.size': 20})
+    ax = plt.subplot()
+    ConfusionMatrixDisplay.from_predictions(y_true=y_test, y_pred=y_pred, ax=ax, cmap="gray", display_labels=['benigno', "maligno"], colorbar=False)
+    ax.set_xlabel("Rótulo previsto", fontsize=16)
+    ax.set_ylabel("Rótulo real", fontsize=16) 
+    ax.plot(colorbar=False)
+    plt.show()
+
 def main(variance=0.01, col=4000):
     
-    for i in range(1, 12):
-        data = pd.read_hdf(f'./fragments/fragment_{i}.h5')
+    data = pd.read_hdf(f'./fragments/fragment_1.h5')
 
-        data = drop_low_var_by_col_100k(data, col=col)
+    data = drop_low_var_by_col_100k(data, col=col)
 
-        data['CLASS'] = np.where(data['vt_detection'] < 4, 0, 1)
-        
-        data = data.drop(columns=['SHA256', 'NOME', 'PACOTE', 'API_MIN', 'API', 'vt_detection', "VT_Malware_Deteccao", "AZ_Malware_Deteccao"])
-        print(f"Testando para o fragmento {i}")
-
-        RF(data, variance)
-
-        break
+    data['CLASS'] = np.where(data['vt_detection'] < 4, 0, 1)
     
+    data = data.drop(columns=['SHA256', 'NOME', 'PACOTE', 'API_MIN', 'API', 'vt_detection', "VT_Malware_Deteccao", "AZ_Malware_Deteccao"])
+
+    RF(data, col)
+
 
 if __name__ == "__main__":
-    main(variance=0.19) # 1972
+    main(col=4000) # 1972
