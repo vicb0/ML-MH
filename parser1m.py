2d312576c1b1133bbc9162664d71afec1b0d9a33
Vicbo <victorbor2001@yahoo.com.br>
2025-04-14 14:40:59 -0300

    Added randomic logic for 1m model instead of sequential

--- parser1m.py
+++ parser1m.py
@@ -1,5 +1,6 @@
 import os
 import math
+import random
 import pandas as pd
 
 import numpy as np
@@ -65,23 +66,35 @@ def build_hdf(overwrite=False, n=1):
     benign = []
     malware = []
 
-    c = 0
+    indeces = []
     for fragment in os.scandir('./fragments1m'):
         if not fragment.name.startswith('fragment_'):
             continue
 
-        data = np.load(f'./fragments1m/fragment_{c + 1}.npz')
+        fragment_index = int(fragment.name.split(".")[0].split("_")[1])
+        indeces.append(fragment_index)
+
+    random.shuffle(indeces)
+
+    c = 0
+    SIZE = 30_000
+    for index in indeces:
+        data = np.load(f'./fragments1m/fragment_{index}.npz')
         data = data['arr']
 
-        if len(malware) < 10_000:
-            malwares = data[vt_detection[c * 100_000:(c + 1) * 100_000] >= 4][:10_000 - len(malware)]
+        if len(malware) < SIZE:
+            malwares = data[vt_detection[(index - 1) * 100_000:index * 100_000] >= 4]
+            filter_indices = np.random.choice(range(len(malwares)), min(SIZE - len(malware), SIZE // len(indeces) + 1), replace=False)
+            malwares = np.take(malwares, filter_indices, axis=0)
             malware.extend(malwares.tolist())
-
-        if len(benign) < 10_000:
-            benigns = data[vt_detection[c * 100_000:(c + 1) * 100_000] < 4][:10_000 - len(benign)]
+            
+        if len(benign) < SIZE:
+            benigns = data[vt_detection[(index - 1) * 100_000:index * 100_000] < 4]
+            filter_indices = np.random.choice(range(len(benigns)), min(SIZE - len(benign), SIZE // len(indeces) + 1), replace=False)
+            benigns = np.take(benigns, filter_indices, axis=0)
             benign.extend(benigns.tolist())
 
-        if len(benign) >= 10_000 and len(malware) >= 10_000:
+        if len(benign) >= SIZE and len(malware) >= SIZE:
             break
 
         c += 1
@@ -93,14 +106,14 @@ def build_hdf(overwrite=False, n=1):
         dtype=np.uint8
     )
 
-    class_df = pd.DataFrame({'CLASS': [0]*10_000 + [1]*10_000})
+    class_df = pd.DataFrame({'CLASS': [0]*SIZE + [1]*SIZE})
     df = pd.concat([df, class_df], axis=1)
 
     s = df.columns.to_series()
     df.columns = s + s.groupby(s).cumcount().astype(str).replace({'0':''})
 
     df.to_hdf(
-        f"./fragments1m/balanced_fragment.h5",
+        f"./fragments1m/balanced_fragment_{SIZE}.h5",
         key="df",
         mode="w",
         format="f"
